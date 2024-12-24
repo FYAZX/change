ebd014a2a88f8f1fe1214a87593d6e97206ecd55
FYAZX <2604027504@qq.com>
2024-12-24 11:10:01 +0800

    Initial commit

---
+++ src/main/java/com/example/change01/Change01Application.java
@@ -0,0 +1,15 @@
+package com.example.change01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Change01Application {
+
+    public static void main(String[] args) {
+        System.out.println("change1");
+        System.out.println("change2");
+        SpringApplication.run(Change01Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/change01/Change01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.change01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Change01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
