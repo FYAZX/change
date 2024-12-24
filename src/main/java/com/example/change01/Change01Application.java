c4ce0aef65ff9764a9061a3b7e4e1df20d6099c7
FYAZX <2604027504@qq.com>
2024-12-24 11:13:54 +0800

    Test GitHub webhook: change1.java

--- src/main/java/com/example/change01/Change01Application.java
+++ src/main/java/com/example/change01/Change01Application.java
@@ -9,6 +9,7 @@ public class Change01Application {
     public static void main(String[] args) {
         System.out.println("change1");
         System.out.println("change2");
+        System.out.println("change3");
         SpringApplication.run(Change01Application.class, args);
     }
 
